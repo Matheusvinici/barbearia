b983c87345999d2cd636d5abe092ba080d432bee
Matheus Vinicius <matheus2vandrade@gmail.com>
2026-07-01 11:35:06 -0300

    Updated 2026-07-01 11:35:06

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,7 +31,7 @@
 
     <a href="{{ navRoute('dashboard') }}" class="nav-item {{ isActive(['admin/dashboard', 'admin/home', 'barbeiro/dashboard', 'dashboard', 'home']) || request()->is('*/dashboard') ? 'active' : '' }}">
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="7" height="7"/><rect x="14" y="3" width="7" height="7"/><rect x="3" y="14" width="7" height="7"/><rect x="14" y="14" width="7" height="7"/></svg>
-        <span>Dashboard</span>
+        <span>Painel Inicial</span>
     </a>
 
     @if($user->can('agendamento.view'))
